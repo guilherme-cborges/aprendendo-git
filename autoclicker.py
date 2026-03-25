7a970077b68646f00072e6f1631186c35e0fcacc
Guilherme <guilherme_cborges@outlook.com.br>
2026-03-24 21:32:23 -0300

    Primeiro commit

---
+++ autoclicker.py
@@ -0,0 +1,25 @@
+import pyautogui
+import time
+import keyboard
+
+print("Auto Clicker - Pressione 'ESC' para parar")
+print("Iniciando em 3 segundos...")
+time.sleep(3)
+
+print("Clicando a cada 1 segundo. Pressione ESC para parar.")
+
+try:
+    while True:
+        # Verifica se ESC foi pressionado
+        if keyboard.is_pressed('esc'):
+            print("\nParado pelo usuário.")
+            break
+        
+        # Clica com o botão esquerdo do mouse
+        pyautogui.click()
+        
+        # Aguarda 1 segundo
+        time.sleep(1)
+        
+except KeyboardInterrupt:
+    print("\nPrograma interrompido.")
